2d23340bca4d3dc74aa31f8ed7d8ada615eecf43
zzj <2541838711@qq.com>
2023-03-14 20:22:46 +0800

    feat：初始化

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # douyin-reptiles
 douyin-reptiles
+
+
+### 步骤一
+```
+cd ./X-Bogus
+python3 server.py
+```
+访问测试 
+127.0.0.1:8787
+### 步骤二
+启动springboot
+127.0.0.1:8088
+
+```
+/
+```
--- src/main/java/com/example/douyin/controller/ReptilesController.java
+++ src/main/java/com/example/douyin/controller/ReptilesController.java
@@ -25,6 +25,14 @@ public class ReptilesController {
         return "ok";
     }
 
+    /***
+     * @author ZZJ
+     * @date 2023/3/14
+     * @param url
+     * @param userAgent
+     * @return com.alibaba.fastjson.JSONObject
+     * @desc  根据url、userAgent 生成X-Bogus
+     */
     @GetMapping("/xbogus")
     public JSONObject getXbogus(String url,String userAgent){
         UrlBuilder builder = UrlBuilder.ofHttp(url, CharsetUtil.CHARSET_UTF_8);
@@ -32,16 +40,37 @@ public class ReptilesController {
         return reptilesService.getXbogus(queryStr, userAgent);
     }
 
+    /***
+     * @author ZZJ
+     * @date 2023/3/14
+     * @return java.lang.String
+     * @desc  利用字节中间接口注册ttwid
+     */
     @GetMapping("/ttwid")
     public String getTTwid(){
         return reptilesService.getTTwid();
     }
 
+    /***
+     * @author ZZJ
+     * @date 2023/3/14
+     * @return java.lang.String
+     * @desc  获取cookie，参数：ttwid=xxx;bd_ticket_guard_client_data=xxx
+     */
     @GetMapping("/cookie")
     public String getCookie(){
         return reptilesService.getCookie();
     }
 
+    /***
+     * @author ZZJ
+     * @date 2023/3/14
+     * @param secUserId
+     * @param maxCursor
+     * @param minCursor
+     * @return com.alibaba.fastjson.JSONObject
+     * @desc  根据secUserId获取喜欢列表信息  example：127.0.0.1:8088/reptiles/getUserFavoriteList/MS4wLjABAAAApmi-USaKOChKt5pX20FjhjJMcH2bFRfh04i2aP-zVlI?maxCursor=1574048283000
+     */
     @GetMapping("/getUserFavoriteList/{secUserId}")
     public JSONObject getUserFavoriteList(@PathVariable("secUserId") String secUserId
             , @RequestParam(required = false,defaultValue = "0") String maxCursor
